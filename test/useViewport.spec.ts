688ff426df0d6947196e9dd2f56759287a26193c
Jakub Biesiada <biesiadajakub@icloud.com>
2020-02-20 17:14:24 +0100

    Add tests

---
+++ test/useViewport.spec.ts
@@ -0,0 +1,23 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import { useViewport } from '../src/useViewport';
+
+describe('useViewport', () => {
+  it('should return window dimensions', () => {
+    const { result } = renderHook(() => useViewport());
+
+    expect(result.current.vh).toBe(768);
+    expect(result.current.vw).toBe(1024);
+  });
+
+  it('should return window dimensions without update on resize', () => {
+    const options = {
+      updateOnResize: false
+    };
+
+    const { result } = renderHook(() => useViewport(options));
+
+    expect(result.current.vh).toBe(768);
+    expect(result.current.vw).toBe(1024);
+  });
+});
